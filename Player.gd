536a186abc858f640bddb87ec49156398cce62c6
Karvalho <renato@karvalho.com.br>
2023-01-07 17:47:32 -0300

    Improved character movement

--- Player.gd
+++ Player.gd
@@ -10,7 +10,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		apply_gravity(delta)
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -25,3 +25,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func apply_gravity(delta):
+	velocity.y += gravity * delta
