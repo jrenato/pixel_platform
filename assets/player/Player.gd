3f9fba653f43c7f6ff3ddef83bfaa7142bc6c7ab
karvalho <renato@karvalho.com.br>
2023-01-11 19:39:36 -0300

    Improved jump

--- assets/player/Player.gd
+++ assets/player/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 60.0 # 300
-const JUMP_VELOCITY = -150.0 # -400
+const SPEED = 120.0 # 300
+const JUMP_VELOCITY = -200.0 # -400
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,8 +13,12 @@ func _physics_process(delta):
 		apply_gravity(delta)
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_select"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("ui_select") and velocity.y < 0:
+			velocity.y = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
