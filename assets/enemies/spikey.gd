21fa867f3d559061034720f3e9eecd1c0ee40397
karvalho <renato@karvalho.com.br>
2023-01-17 07:51:35 -0300

    Added spikey enemy

--- assets/enemies/spikey.gd
+++ assets/enemies/spikey.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 var direction : Vector2 = Vector2.LEFT
 
-@onready var edge_check : RayCast2D = $EdgeCheck
+@onready var edge_check_left : RayCast2D = $EdgeCheckLeft
+@onready var edge_check_right : RayCast2D = $EdgeCheckRight
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	velocity = Vector2.ZERO
@@ -10,10 +12,12 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var found_wall : bool = is_on_wall()
-	var found_edge : bool = not edge_check.is_colliding()
+	var found_edge : bool = not edge_check_left.is_colliding() or not edge_check_right.is_colliding()
 
 	if found_wall or found_edge:
 		direction *= -1
+
+	sprite.flip_h = direction.x > 0
 
 	velocity = direction * 25
 	move_and_slide()
