e1bfa78c2e5380504a47877e8202f9c7268d8f42
renato <renato@karvalho.com.br>
2023-01-20 09:38:52 -0300

    Improved moving spike tooling

--- assets/enemies/moving_spike.gd
+++ assets/enemies/moving_spike.gd
@@ -7,25 +7,21 @@ enum ANIMATION_TYPE { LOOP, BOUNCE }
 
 @export var speed : int = 50 : 
 	set(value):
-		animation_type = value
-		# Check this issue
-		# https://github.com/godotengine/godot-proposals/issues/325
-		# It should be fixed with @onready setters and getters
-		if not is_inside_tree():
-			await ready
-		animation_player.playback_speed = speed / curve.get_baked_length() 
+		speed = value
+		# Set value if running inside editor (tool)
+		if Engine.is_editor_hint():
+			print("Updating speed in editor: ", speed)
+			animation_player.playback_speed = speed / curve.get_baked_length() 
 	get:
-		return animation_type
+		return speed
 
 @export var animation_type : ANIMATION_TYPE = ANIMATION_TYPE.LOOP :
 	set(value):
 		animation_type = value
-		# Check this issue
-		# https://github.com/godotengine/godot-proposals/issues/325
-		# It should be fixed with @onready setters and getters
-		if not is_inside_tree():
-			await ready
-		set_animation_type()
+		# Set value if running inside editor (tool)
+		if Engine.is_editor_hint():
+			print("Updating animation type in editor")
+			set_animation_type()
 	get:
 		return animation_type
 
