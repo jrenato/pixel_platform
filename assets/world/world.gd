e367637babd5de1db6194497b7d96b9a7fb38932
Karvalho <renato@karvalho.com.br>
2023-01-22 08:45:32 -0300

    Fixed player var bug in world

--- assets/world/world.gd
+++ assets/world/world.gd
@@ -4,7 +4,7 @@ const player_scene = preload("res://assets/player/player.tscn")
 
 var spawn_position : Vector2 = Vector2.ZERO
 
-@onready var player : CharacterBody2D = $Player
+@onready var player : Player = $Player
 @onready var camera : Camera2D = $Camera2D
 @onready var respawn_timer : Timer = $RespawnTimer
 
@@ -21,7 +21,7 @@ func _on_player_died() -> void:
 	respawn_timer.start()
 	await respawn_timer.timeout
 
-	var player : Player = player_scene.instantiate()
+	player = player_scene.instantiate()
 	add_child(player)
 	player.position = spawn_position
 	player.connect_camera(camera)
