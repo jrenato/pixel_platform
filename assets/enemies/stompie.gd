3781019e8095cd6a5a83de5a7eb4c8f5195de6b0
karvalho <renato@karvalho.com.br>
2023-01-23 15:59:48 -0300

    Added partial tool support to stompie enemy

--- assets/enemies/stompie.gd
+++ assets/enemies/stompie.gd
@@ -1,14 +1,33 @@
+#@tool
+# TODO: is it possible to detect collisions with a tilemap in editor?
 extends Node2D
 
 enum {HOVER, FALL, LAND, RISE}
 
 var state := HOVER
 
-@export var wait_time : float = 1.0
+@export var active : bool = true :
+	set(value):
+		active = value
+		if Engine.is_editor_hint():
+			PhysicsServer2D.set_active(active)
+			timer.start()
+	get:
+		return active
+
+@export var wait_time : float = 1.0 :
+	set(value):
+		wait_time = value
+		# Set value if running inside editor (tool)
+		if Engine.is_editor_hint():
+			timer.wait_time = wait_time
+	get:
+		return wait_time
+
 @export var fall_speed : int = 200
 @export var rise_speed : int = 50
 
-@onready var start_position : Vector2 = global_position
+@onready var start_position : Vector2 = global_position # Vector2(558, 522)
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 @onready var particles : GPUParticles2D = $GPUParticles2D
 @onready var raycast : RayCast2D = $RayCast2D
@@ -17,6 +36,7 @@ var state := HOVER
 
 func _ready() -> void:
 	timer.wait_time = wait_time
+	start_position = global_position # Vector2(558, 522)
 
 
 func _physics_process(delta : float) -> void:
@@ -28,7 +48,7 @@ func _physics_process(delta : float) -> void:
 
 
 func hover_state(_delta : float) -> void:
-	if timer.is_stopped():
+	if timer.is_stopped() and active:
 		timer.start()
 
 
@@ -58,7 +78,9 @@ func _on_timer_timeout():
 		HOVER:
 			state = FALL
 			animated_sprite.play("Falling")
+			print("Falling")
 		LAND:
 			state = RISE
 			animated_sprite.play("Rising")
 			particles.emitting = false
+			print("Rising")
