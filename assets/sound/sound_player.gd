36f5d131e854ba1241eef6bda619c7ce1af87d05
karvalho <renato@karvalho.com.br>
2023-01-24 19:58:10 -0300

    Added music

--- assets/sound/sound_player.gd
+++ assets/sound/sound_player.gd
@@ -1,15 +1,20 @@
 extends Node
 
-const HURT = preload("res://assets/sound/sounds/hurt.wav")
-const JUMP = preload("res://assets/sound/sounds/jump2.wav")
-const BOOM = preload("res://assets/sound/sounds/boom.wav")
-const LOSE = preload("res://assets/sound/sounds/lose.wav")
-const RESPAWN = preload("res://assets/sound/sounds/respawn.wav")
-const SCENETRANSITION = preload("res://assets/sound/sounds/scene_transition.wav")
-const CHECKPOINT = preload("res://assets/sound/sounds/checkpoint.wav")
+const HURT : AudioStream = preload("res://assets/sound/sounds/hurt.wav")
+const JUMP : AudioStream = preload("res://assets/sound/sounds/jump2.wav")
+const BOOM : AudioStream = preload("res://assets/sound/sounds/boom.wav")
+const LOSE : AudioStream = preload("res://assets/sound/sounds/lose.wav")
+const RESPAWN : AudioStream = preload("res://assets/sound/sounds/respawn.wav")
+const SCENETRANSITION : AudioStream = preload("res://assets/sound/sounds/scene_transition.wav")
+const CHECKPOINT : AudioStream = preload("res://assets/sound/sounds/checkpoint.wav")
+
+const ADVENTURE : AudioStream = preload("res://assets/sound/songs/Adventures in Adventureland.mp3")
+const DUNGEON : AudioStream= preload("res://assets/sound/songs/8bit Dungeon Level.mp3")
 
 @onready var audio_players : Node = $AudioPlayers
+@onready var song_audio_player: AudioStreamPlayer = $SongAudioStreamPlayer
 
+var current_song : String = ""
 
 func play_sound(sound : AudioStream) -> void:
 	for audio_stream_player in audio_players.get_children():
@@ -17,3 +22,19 @@ func play_sound(sound : AudioStream) -> void:
 			audio_stream_player.stream = sound
 			audio_stream_player.play()
 			break
+
+
+func play_song(song : String) -> void:
+	if song.is_empty() or current_song == song:
+		return
+
+	current_song = song
+
+	# Default
+	song_audio_player.stream = ADVENTURE
+
+	match song:
+		"Dungeon":
+			song_audio_player.stream = DUNGEON
+	
+	song_audio_player.play()
--- assets/world/level.gd
+++ assets/world/level.gd
@@ -8,6 +8,9 @@ var spawn_position : Vector2 = Vector2.ZERO
 @onready var camera : Camera2D = $Camera2D
 @onready var respawn_timer : Timer = $RespawnTimer
 
+#@export_enum("Adventure", "Dungeon") var song : String
+
+@export var song : String = "Adventure"
 
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.LIGHT_BLUE)
@@ -15,6 +18,7 @@ func _ready() -> void:
 	Events.connect("player_died", _on_player_died)
 	Events.connect("update_checkpoint", _on_update_checkpoint)
 	spawn_position = player.position
+	SoundPlayer.play_song(song)
 
 
 func _on_player_died() -> void:
