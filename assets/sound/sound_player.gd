4cd27f37990987fa0e7aca5156eef94e742e5762
karvalho <renato@karvalho.com.br>
2023-01-24 19:18:52 -0300

    Added scene transition sound

--- assets/sound/sound_player.gd
+++ assets/sound/sound_player.gd
@@ -5,6 +5,7 @@ const JUMP = preload("res://assets/sound/sounds/jump2.wav")
 const BOOM = preload("res://assets/sound/sounds/boom.wav")
 const LOSE = preload("res://assets/sound/sounds/lose.wav")
 const RESPAWN = preload("res://assets/sound/sounds/respawn.wav")
+const SCENETRANSITION = preload("res://assets/sound/sounds/scene_transition.wav")
 const CHECKPOINT = preload("res://assets/sound/sounds/checkpoint.wav")
 
 @onready var audio_players : Node = $AudioPlayers
--- assets/world/door/door.gd
+++ assets/world/door/door.gd
@@ -7,5 +7,5 @@ func _on_body_entered(body: Node2D) -> void:
 	if not body is Player or target_level_path.is_empty():
 		return
 
-	SceneTransition.change_scene(target_level_path)
-
+	SoundPlayer.play_sound(SoundPlayer.SCENETRANSITION)
+	SceneTransition.change_scene(target_level_path, "clouds")
