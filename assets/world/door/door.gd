88ee975628acd742198e7a7efd5b32e7a57be864
karvalho <renato@karvalho.com.br>
2023-01-23 23:27:53 -0300

    Added level transitions

--- assets/world/door/door.gd
+++ assets/world/door/door.gd
@@ -7,4 +7,12 @@ func _on_body_entered(body: Node2D) -> void:
 	if not body is Player or target_level_path.is_empty():
 		return
 
+	Transitions.play_exit_level()
+	await Transitions.animation_player.animation_finished
+	get_tree().paused = true
+
+	Transitions.play_enter_level()
+	await Transitions.animation_player.animation_finished
+	get_tree().paused = false
+
 	get_tree().change_scene_to_file(target_level_path)
