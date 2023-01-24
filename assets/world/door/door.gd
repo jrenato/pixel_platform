dc0b760aeb625a4d7e5692f764e8b71a098a5736
karvalho <renato@karvalho.com.br>
2023-01-24 08:14:27 -0300

    Small tweaks to transition (still bugged)

--- assets/world/door/door.gd
+++ assets/world/door/door.gd
@@ -9,10 +9,9 @@ func _on_body_entered(body: Node2D) -> void:
 
 	Transitions.play_exit_level()
 	await Transitions.animation_player.animation_finished
-	get_tree().paused = true
 
+	get_tree().paused = true
 	Transitions.play_enter_level()
-	await Transitions.animation_player.animation_finished
 	get_tree().paused = false
 
 	get_tree().change_scene_to_file(target_level_path)
