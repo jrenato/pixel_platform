5af12323ef8c74c7b7c6903565019a58a75284a1
karvalho <renato@karvalho.com.br>
2023-01-24 19:12:22 -0300

    Refactored SceneTransition

--- assets/world/door/door.gd
+++ assets/world/door/door.gd
@@ -7,11 +7,5 @@ func _on_body_entered(body: Node2D) -> void:
 	if not body is Player or target_level_path.is_empty():
 		return
 
-	Transitions.play_exit_level()
-	await Transitions.animation_player.animation_finished
+	SceneTransition.change_scene(target_level_path)
 
-	get_tree().paused = true
-	Transitions.play_enter_level()
-	get_tree().paused = false
-
-	get_tree().change_scene_to_file(target_level_path)
---
+++ assets/world/scene_transition.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func change_scene(target : String, type: String = 'dissolve') -> void:
+	if type == 'dissolve':
+		transition_dissolve(target)
+	else:
+		transition_clouds(target)
+
+
+func transition_dissolve(target: String) -> void:
+	animation_player.play("dissolve")
+	await animation_player.animation_finished
+	get_tree().change_scene_to_file(target)
+	animation_player.play_backwards("dissolve")
+
+
+func transition_clouds(target: String) -> void:
+	animation_player.play("clouds_in")
+	await animation_player.animation_finished
+	get_tree().change_scene_to_file(target)
+	animation_player.play("clouds_out")
