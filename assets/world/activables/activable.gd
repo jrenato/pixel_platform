d58b999aecd9e45342260ac5613bd598dd7528d0
renato <renato@karvalho.com.br>
2023-02-03 17:39:11 -0300

    More code to moving wall

--- assets/world/activables/activable.gd
+++ assets/world/activables/activable.gd
@@ -1,11 +1,28 @@
 class_name Activable
 extends Node2D
 
-var block : Resource = preload("res://assets/world/blocks/block.tscn")
+@onready var block_group: Node2D = $BlockGroup
+
+var block_scene : Resource = preload("res://assets/world/blocks/block.tscn")
+
+@export var size : int = 1
 
 
 func _ready() -> void:
-	pass
+	remove_all_blocks()
+	add_blocks()
+
+
+func add_blocks() -> void:
+	for i in size:
+		var block = block_scene.instantiate()
+		block_group.add_child(block)
+
+
+func remove_all_blocks() -> void:
+	var block_children : Array[Node] = block_group.get_children()
+	for block_child in block_children:
+		block_group.remove_child(block_child)
 
 
 func activate() -> void:
--- assets/world/activators/pressure_button.gd
+++ assets/world/activators/pressure_button.gd
@@ -1,8 +1,13 @@
 extends Activator
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
 @export var activated : bool = false
+@export_enum("Up", "Down", "Left", "Right") var direction : int = 0
+
+func _ready() -> void:
+	#TODO: Find out why this is necessary
+	animated_sprite.play_backwards("trigger")
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
