715cf1686f906187b3979d7b731fdf729c00630e
renato <renato@karvalho.com.br>
2023-02-03 11:36:45 -0300

    Refactored tileset files

--- assets/game_state/resources/level_data.gd
+++ assets/game_state/resources/level_data.gd
@@ -8,6 +8,11 @@ func set_collectible_state(level_name : String, item_name : String, collected : 
 	# True: collected
 	# False: not collected
 	if not level_name.is_empty():
+		if not levels.has(level_name):
+			levels[level_name] = {}
+		if not levels[level_name].has("items"):
+			levels[level_name]["items"] = {}
+
 		levels[level_name]["items"][item_name] = collected
 
 
@@ -15,8 +20,13 @@ func get_collectible_state(level_name : String, item_name : String) -> bool:
 	# True: collected
 	# False: not collected
 	if not level_name.is_empty():
-		if levels[level_name]["items"].has(item_name):
+		if not levels.has(level_name):
+			return false
+		elif not levels[level_name].has("items"):
+			return false
+		elif not levels[level_name]["items"].has(item_name):
+			return false
+		else:
 			return levels[level_name]["items"][item_name]
-	# If level or collectible not found, return false
-	# Otherwise, object will be destroyed as soon as level_data is created
+
 	return false
