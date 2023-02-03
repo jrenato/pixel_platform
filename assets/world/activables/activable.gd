734b23e2a64711a6baeec4bdaa245c117bad40d6
renato <renato@karvalho.com.br>
2023-02-03 17:07:30 -0300

    Started implementation of moving wall

---
+++ assets/world/activables/activable.gd
@@ -0,0 +1,16 @@
+class_name Activable
+extends Node2D
+
+var block : Resource = preload("res://assets/world/blocks/block.tscn")
+
+
+func _ready() -> void:
+	pass
+
+
+func activate() -> void:
+	pass
+
+
+func deactivate() -> void:
+	pass
