2bc0053cc24a337f6577e5ec3bd0d311b900f4e4
karvalho <renato@karvalho.com.br>
2023-02-16 17:46:43 -0300

    Added lock activator

---
+++ assets/world/activators/lock.gd
@@ -0,0 +1,18 @@
+extends Activator
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@export var key_id : String = ""
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player and not key_id.is_empty():
+		var key_item : Item = GameManager.inventory_manager.get_item_with_id(key_id)
+		if key_item != null:
+			GameManager.inventory_manager.remove_selected_item()
+			Events.emit_signal("update_inventory_ui")
+			animation_player.play("unlock")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	activate()
